9cd4a36d112ae36b43997b7dce74917f11762015
Alyona1588 <Shuremanochka@bk.ru>
2024-03-22 22:20:55 +0300

    6-week fux bug
    6-week fux bug:
    - display logo in page !=/
    - fix voting

---
+++ Скачано с Практикум/notepade.js
@@ -0,0 +1,216 @@
+
+[
+    {
+        "id": 1,
+        "title": "Crystal Keeper",
+        "developer": "Lonely Baobab",
+        "description": "Управляй боевым дроном, чтобы любой ценой защитить кристалл от враждебных космо-слизней.",
+        "image": "https://code.s3.yandex.net/teens/pindie-games/cristal-keeper/cover.png",
+        "link": "https://code.s3.yandex.net/teens/pindie-games/cristal-keeper/game/index.html",
+        "published_at": "2024-01-22T16:48:07.159Z",
+        "created_at": "2024-01-22T16:46:01.600Z",
+        "updated_at": "2024-02-21T15:32:55.880Z",
+        "user": null,
+        "categories": [
+            {
+                "id": 1,
+                "name": "new",
+                "published_at": "2024-01-22T16:46:15.004Z",
+                "created_at": "2024-01-22T16:46:10.263Z",
+                "updated_at": "2024-01-22T16:46:15.015Z"
+            },
+            {
+                "id": 3,
+                "name": "pixel",
+                "published_at": "2024-01-22T16:46:51.075Z",
+                "created_at": "2024-01-22T16:46:46.687Z",
+                "updated_at": "2024-01-22T16:46:51.084Z"
+            },
+            {
+                "id": 4,
+                "name": "TDS",
+                "published_at": "2024-01-22T16:47:10.085Z",
+                "created_at": "2024-01-22T16:47:05.518Z",
+                "updated_at": "2024-01-22T16:47:10.094Z"
+            }
+        ],
+        "users_permissions_users": [
+            {
+                "id": 1,
+                "username": "Aski",
+                "email": "aski@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:03:04.231Z",
+                "updated_at": "2024-02-03T16:25:05.998Z",
+                "games": null
+            },
+            {
+                "id": 2,
+                "username": "Цэ-эс-эсочка",
+                "email": "cssochka@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:03:53.970Z",
+                "updated_at": "2024-02-03T16:25:28.675Z",
+                "games": null
+            },
+            {
+                "id": 3,
+                "username": "Jav-Jav",
+                "email": "jav-jav@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:04:33.565Z",
+                "updated_at": "2024-02-03T16:25:16.962Z",
+                "games": 5
+            },
+            {
+                "id": 484,
+                "username": "mercury_feeb2c38",
+                "email": "mercury_feeb2c38@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": null,
+                "role": 1,
+                "created_at": "2024-03-12T16:15:42.493Z",
+                "updated_at": "2024-03-12T16:15:42.817Z",
+                "games": null
+            },
+            {
+                "id": 718,
+                "username": "mars_217c8e76",
+                "email": "mars_217c8e76@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": null,
+                "role": 1,
+                "created_at": "2024-03-21T20:06:55.187Z",
+                "updated_at": "2024-03-21T20:06:55.312Z",
+                "games": null
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "title": "G.U.N.N.E.R.",
+        "developer": "IDKWIAm",
+        "description": "Продержись как можно дольше, отбиваясь от врагов, которых будет становиться всё больше.",
+        "image": "https://code.s3.yandex.net/teens/pindie-games/gunner/cover.png",
+        "link": "https://code.s3.yandex.net/teens/pindie-games/gunner/game/index.html",
+        "published_at": "2024-01-22T16:53:10.498Z",
+        "created_at": "2024-01-22T16:53:05.868Z",
+        "updated_at": "2024-01-30T13:40:15.556Z",
+        "user": null,
+        "categories": [
+            {
+                "id": 1,
+                "name": "new",
+                "published_at": "2024-01-22T16:46:15.004Z",
+                "created_at": "2024-01-22T16:46:10.263Z",
+                "updated_at": "2024-01-22T16:46:15.015Z"
+            },
+            {
+                "id": 2,
+                "name": "popular",
+                "published_at": "2024-01-22T16:46:35.204Z",
+                "created_at": "2024-01-22T16:46:30.600Z",
+                "updated_at": "2024-01-22T16:46:35.213Z"
+            },
+            {
+                "id": 4,
+                "name": "TDS",
+                "published_at": "2024-01-22T16:47:10.085Z",
+                "created_at": "2024-01-22T16:47:05.518Z",
+                "updated_at": "2024-01-22T16:47:10.094Z"
+            },
+            {
+                "id": 5,
+                "name": "shooter",
+                "published_at": "2024-01-22T16:47:26.118Z",
+                "created_at": "2024-01-22T16:47:21.262Z",
+                "updated_at": "2024-01-22T16:47:26.131Z"
+            }
+        ],
+        "users_permissions_users": [
+            {
+                "id": 1,
+                "username": "Aski",
+                "email": "aski@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:03:04.231Z",
+                "updated_at": "2024-02-03T16:25:05.998Z",
+                "games": null
+            },
+            {
+                "id": 2,
+                "username": "Цэ-эс-эсочка",
+                "email": "cssochka@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:03:53.970Z",
+                "updated_at": "2024-02-03T16:25:28.675Z",
+                "games": null
+            },
+            {
+                "id": 3,
+                "username": "Jav-Jav",
+                "email": "jav-jav@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": false,
+                "role": 3,
+                "created_at": "2024-01-22T17:04:33.565Z",
+                "updated_at": "2024-02-03T16:25:16.962Z",
+                "games": 5
+            },
+            {
+                "id": 120,
+                "username": "jupiter_052220a1",
+                "email": "jupiter_052220a1@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": null,
+                "role": 1,
+                "created_at": "2024-02-27T10:04:47.427Z",
+                "updated_at": "2024-02-27T10:04:47.470Z",
+                "games": null
+            },
+            {
+                "id": 718,
+                "username": "mars_217c8e76",
+                "email": "mars_217c8e76@example.com",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": null,
+                "role": 1,
+                "created_at": "2024-03-21T20:06:55.187Z",
+                "updated_at": "2024-03-21T20:06:55.312Z",
+                "games": null
+            },
+            {
+                "id": 751,
+                "username": "tester007",
+                "email": "tester007@mail.ru",
+                "provider": "local",
+                "confirmed": true,
+                "blocked": null,
+                "role": 1,
+                "created_at": "2024-03-22T11:51:51.123Z",
+                "updated_at": "2024-03-22T11:51:51.126Z",
+                "games": null
+            }
+        ]
+    }
+]
