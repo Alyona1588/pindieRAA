7b7b0d829a2a912cbffad33cc9c0681d51662454
Alyona1588 <Shuremanochka@bk.ru>
2024-05-06 22:37:13 +0300

    Приложение настроено на работу с локальным сервером

--- pindieraa/app/api/config.js
+++ pindieraa/app/api/config.js
@@ -1,12 +1,15 @@
-export const BASE_URL = 'https://api-code-2.practicum-team.ru';
+// УРЛ Яндекс
+// export const BASE_URL = 'https://api-code-2.practicum-team.ru';
+
+// УРЛ локальный
+export const BASE_URL = "http://localhost:3001/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   id: `${BASE_URL}/id`,
-  auth:  `${BASE_URL}/auth/local`,
-  me:  `${BASE_URL}/users/me`,
-}; 
-
+  auth: `${BASE_URL}/auth/local`,
+  me: `${BASE_URL}/users/me`,
+};
 
 // https://api-code-2.practicum-team.ru/games
 
