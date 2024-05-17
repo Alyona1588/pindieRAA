cbab6e7693986303ca3da00348c22d101f578da8
Alyona1588 <Shuremanochka@bk.ru>
2024-05-17 18:37:12 +0300

    API переключен на продуктив

--- pindieraa/app/api/config.js
+++ pindieraa/app/api/config.js
@@ -2,7 +2,9 @@
 // export const BASE_URL = 'https://api-code-2.practicum-team.ru';
 
 // УРЛ локальный
-export const BASE_URL = "http://localhost:3001/api";
+//export const BASE_URL = "http://localhost:3001/api"; // для локального сервера
+export const BASE_URL =
+  "https://shuremanochka.nomoredomainswork.ru/admin/dashboard"; // для продуктива
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
