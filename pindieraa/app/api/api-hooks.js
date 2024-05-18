06e4e5ad3a2a68a2e886201b89f58b6535d2f962
Alyona1588 <Shuremanochka@bk.ru>
2024-05-18 14:45:13 +0300

    fix critical error: проверка наличия данных в массиве

--- pindieraa/app/api/api-hooks.js
+++ pindieraa/app/api/api-hooks.js
@@ -1,18 +1,21 @@
-'use client';
-import {useState, useEffect} from 'react';
-import {getNormalizedGamesDataByCategory} from './api-utils';
-import { endpoints } from './config';
+"use client";
+import { useState, useEffect } from "react";
+import { getNormalizedGamesDataByCategory } from "./api-utils";
+import { endpoints } from "./config";
 
 export const useGetDataByCategory = (endpoint, category) => {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-      async function fetchData() {
-        const data = await getNormalizedGamesDataByCategory(endpoint, category);
-        setData(data);
-      }
-      fetchData();
-    }, []);
-    return data;
-  }; 
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getNormalizedGamesDataByCategory(endpoint, category);
+      setData(data); //было
+      // стало
+      //data ? setData(data) : return;
 
-  
+      //Пример
+      //isResponseOk(data) ? normalizeData(data) : data;
+    }
+    fetchData();
+  }, []);
+  return data;
+};
